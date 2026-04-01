2a89f932e898e53d73d0d374297ede01dcbe5fcd
Rogério Fernandes <roferrock@gmail.com>
2026-03-31 21:09:50 -0300

    fix(server): update message handling changes

--- server.py
+++ server.py
@@ -4,33 +4,33 @@ import threading
 HOST = 'localhost'
 PORT = 55555
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((HOST, PORT))
-server_socket.listen()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind((HOST, PORT))
+server.listen()
 
 rooms = {}
 
 def broadcast(room, message):
-    for client in rooms[room]:
+    for i in rooms[room]:
         if isinstance(message, str):
             message = message.encode()
-        client.send(message)
+        i.send(message)
 
-def send_message(name, room, client_socket):
+def send_message(name, room, client):
     while True:
-        message = client_socket.recv(1024)
-        message = f'{name}: {message.decode()}\n'
+        message = client.recv(1024)
+        message = f"{name}: {message.decode()}\n"
         broadcast(room, message)
 
 while True:
-    client_socket, addr = server_socket.accept()
-    client_socket.send(b'Nome da Sala: ')
-    room = client_socket.recv(1024).decode()
-    name = client_socket.recv(1024).decode()
+    client, addr = server.accept()
+    client.send(b'SALA')
+    room = client.recv(1024).decode()
+    name = client.recv(1024).decode()
     if room not in rooms.keys():
         rooms[room] = []
-    rooms[room].append(client_socket)
+    rooms[room].append(client)
     print(f'{name} entrou na sala {room}! INFO: {addr}')
-    broadcast(room, f'{name} entrou na sala!'.encode())
-    thread = threading.Thread(target=send_message, args=(name, room, client_socket))
+    broadcast(room, f'{name} entrou na sala!\n'.encode())
+    thread = threading.Thread(target=send_message, args=(name, room, client))
     thread.start()
